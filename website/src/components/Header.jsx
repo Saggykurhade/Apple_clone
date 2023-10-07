cf7a669cf4cb2df4e97437f15ab340bce0ac6ac0
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-10-07 17:16:31 +0530

    updates

---
+++ website/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
