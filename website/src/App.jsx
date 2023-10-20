d5c208978d7f08362aedf625c4c4a41326284d8c
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-10-20 20:11:36 +0530

    updates

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Store from './components/Store';
 import IPhone15Pro from './components/IPhone15Pro';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Homepage />} />
       <Route exact path='/store' element={<Store />} />
       <Route exact path='/IPhone15Pro' element={<IPhone15Pro />} />
+      <Route exact path='/Cart' element={<Cart />} />
     </Routes>
   );
 }
---
+++ website/src/components/Cart.jsx
@@ -0,0 +1,202 @@
+import React from 'react'
+import Header from './HeaderStore';
+import Footer from './Footer';
+import './cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            <Header />
+
+            <div className='card-cart'>
+                <span>Save up to ₹10000.00 instantly on selected Mac models with HDFC Bank Credit Cards. Footnote §§</span><br />
+                <span>Plus No Cost EMI from most leading banks. Footnote § </span>
+                <span> See offers {'>'}</span>
+            </div>
+
+            <div className='container-cart'>
+
+                <div className="check-out-cart">
+                    <h1>Your bag total is ₹121600.00 or ₹18962.00/mo.^</h1>
+                    <button>Check Out</button>
+                </div>
+
+                <div className="product-cart">
+                    <div className="prod-cart-1">
+
+                        <div className="prod-flex-cart">
+                            <div className="prod-img-cart">
+                                <img width="200px" src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=400&hei=400&fmt=jpeg&qlt=95&fit=constrain&.v=1664472289661" alt="" />
+                            </div>
+
+                            <div className="prod-detail-cart">
+                                <div className="prod-name-cart-1">
+                                    <h2>
+                                        13-inch MacBook Air with M1 chip - Space Grey
+                                    </h2>
+                                    <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                    </select>
+                                    <h3>₹99900.00</h3>
+                                </div>
+
+                                <div className="emi-prod-cart">
+                                    <p>Pay 15% pa for 6 months:^</p>
+                                    <p>Get up to ₹11891.00 savings with eligible card
+                                        ₹15317.00/mo.</p>
+                                </div>
+
+                                <div className="show-prod-cart-1">
+                                    <h3>Show product details</h3>
+                                    <h3>Remove</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="prod-detail-cart-list-2">
+                            <div className="prod-name-cart-1-list2">
+                                <h3><i class="fa-brands fa-apple" style={{ color: "#fa0000" }}></i>
+                                    AppleCare+ for MacBook Air (M1)
+                                </h3>
+                                <h4 >₹17900.00</h4>
+                            </div>
+                            <div className="emi-prod-cart">
+                                <p>Pay 15% pa for 6 months:^</p>
+                                <p>
+                                    ₹2500.00/mo.</p>
+                            </div>
+                            <div className="show-prod-cart-1-list2">
+                                <h3>Remove</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="product-cart">
+                    <div className="prod-cart-2">
+
+                        <div className="prod-flex-cart">
+                            <div className="prod-img-cart">
+                                <img width="200px" src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MM0A3?wid=400&hei=400&fmt=jpeg&qlt=95&fit=constrain&.v=1632956386000" alt="" />
+                            </div>
+
+                            <div className="prod-detail-cart">
+                                <div className="prod-name-cart-1">
+                                    <h2>
+                                        13-inch MacBook Air with M1 chip - Space Grey
+                                    </h2>
+                                    <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                    </select>
+                                    <h3 >₹99900.00</h3>
+                                </div>
+                                <div className="emi-prod-cart">
+                                    <p>Pay 15% pa for 6 months:^</p>
+                                    <p>Get up to ₹11891.00 savings with eligible card
+                                        ₹15317.00/mo.</p>
+                                </div>
+                                <div className="show-prod-cart-1">
+                                    <h3>Show product details</h3>
+                                    <h3>Remove</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="prod-detail-cart-list-2">
+
+                            <div className="prod-name-cart-1-list2">
+                                <h3><i class="fa-brands fa-apple" style={{ color: "#fa0000" }}></i>
+                                    AppleCare+ for MacBook Air (M1)
+                                </h3>
+                                <h4>₹17900.00</h4>
+                            </div>
+                            <div className="emi-prod-cart">
+                                <p>Pay 15% pa for 6 months:^</p>
+                                <p>₹2500.00/mo.</p>
+                            </div>
+                            <div className="show-prod-cart-1-list2">
+
+                                <h3>Remove</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="product-cart">
+                    <div className="prod-cart-2">
+
+                        <div className="prod-flex-cart">
+                            <div className="prod-img-cart">
+                                <img width="200px" src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MJ1M2?wid=400&hei=400&fmt=jpeg&qlt=95&fit=constrain&.v=1591650473000" alt="" />
+                            </div>
+
+                            <div className="prod-detail-cart">
+                                <div className="prod-name-cart-1">
+                                    <h2>
+                                        13-inch MacBook Air with M1 chip - Space Grey
+                                    </h2>
+                                    <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                    </select>
+                                    <h3>₹99900.00</h3>
+                                </div>
+                                <div className="emi-prod-cart">
+                                    <p>Pay 15% pa for 6 months:^</p>
+                                    <p>Get up to ₹11891.00 savings with eligible card
+                                        ₹15317.00/mo.</p>
+                                </div>
+                                <div className="show-prod-cart-1">
+                                    <h3>Show product details</h3>
+                                    <h3>Remove</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="prod-detail-cart-list-2">
+                            <div className="prod-name-cart-1-list2">
+                                <h3><i class="fa-brands fa-apple" style={{ color: "#fa0000" }}></i>
+                                    AppleCare+ for MacBook Air (M1)
+                                </h3>
+                                <h4 >₹17900.00</h4>
+                            </div>
+
+                            <div className="emi-prod-cart">
+                                <p>Pay 15% pa for 6 months:^</p>
+                                <p>₹2500.00/mo.</p>
+                            </div>
+                            <div className="show-prod-cart-1-list2">
+                                <h3>Remove</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+          <div className="horizontal-line"></div>
+
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Cart;
