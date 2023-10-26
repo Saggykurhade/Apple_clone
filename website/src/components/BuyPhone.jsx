6a9139bbeabb5c0606648ce3be5f90ad8e18162d
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-10-26 20:02:41 +0530

    updates

--- website/src/components/BuyPhone.jsx
+++ website/src/components/BuyPhone.jsx
@@ -8,33 +8,28 @@ const BuyPhone = () => {
     const [product, setProduct] = useState([
         {
             name: "iPhone 15 Pro & iPhone 15 Pro Max",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312",
-            EMI: "From ₹21483.00/mo. with instant savings§§ and No Cost EMI§ or ₹134900.00‡"
+            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290312",
+            EMI: "From $999 or $41.62/mo. for 24 mo.*"
         },
         {
             name: "iPhone 15 & iPhone 15 Plus",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15hero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290559",
-            EMI: "From ₹12483.00/mo. with instant savings§§ and No Cost EMI§ or ₹79900.00‡"
+            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone15hero-202309?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290559",
+            EMI: "From $799 or $33.29/mo. for 24 mo.*"
         },
         {
             name: "Phone 14 & iPhone 14 Plus",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14-202209?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290654",
-            EMI: "From ₹10983.00/mo. with instant savings§§ and No Cost EMI§ or ₹69900.00‡"
+            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14-202209?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1693086290654",
+            EMI: "From $699 or $29.12/mo. for 24 mo.*"
         },
         {
             name: "iPhone 13",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone13hero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1692912410963",
-            EMI: "From ₹9483.00/mo. with instant savings§§ and No Cost EMI§ or ₹59900.00‡"
+            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone13hero-202309?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1692912410963",
+            EMI: "From $599 or $24.95/mo. for 24 mo.*"
         },
         {
             name: "iPhone SE",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1646415838921",
-            EMI: "From ₹7983.00/mo. with instant savings§§ and No Cost EMI§ or ₹49900.00‡"
-        },
-        {
-            name: "iPhone SE",
-            image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1646415838921",
-            EMI: "From ₹7983.00/mo. with instant savings§§ and No Cost EMI§ or ₹49900.00‡"
+            image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646415838921",
+            EMI: "From $429 or $17.87/mo. for 24 mo.*"
         }
     ])
 
@@ -46,10 +41,10 @@ const BuyPhone = () => {
         <div className='Productscreen'>
 
             <HeaderStore />
+            
             <div className="discount-buy">
                 <div className="dis-text-buy">
-                    <h5>Get up to ₹8000.00 instant savings on selected products with eligible HDFC Bank cards.*</h5>
-                    <a href="#">shop now</a>
+                    <p>Get $200–$650 in credit toward iPhone 15 or iPhone 15 Pro when you trade in an iPhone 11 or higher.2</p>
                 </div>
             </div>
 
@@ -67,107 +62,31 @@ const BuyPhone = () => {
             <div className="title-buy">
                 <ul>
                     <li><b><u>All Models</u></b></li>
-                    <li>Festive specials</li>
                     <li>Shopping Guides</li>
-                    <li>Accessories</li>
+                    <li>Ways to Save</li>
                     <li>set and support</li>
+                    <li>Accessories</li>
+                    <li>The iPhone Experience</li>
                 </ul>
             </div>
 
             <div className="latest-pro-buy">
                 <div className="latest-pro-head-buy">
-                    <h2>All models. <span>Take your pick.</span> </h2>
+                    <h2>All models. <span>Take your pick.</span></h2>
                 </div>
+
                 <div className="latest-pro-list-buy" onClick={() => setBuyIphone15Pro(!BuyIphone15Pro)}  >
                     {product.length &&
                         <div className='single-flx-buy'>{product.map((pro) => (
                             <div className='flexbox-buy'>
-                                <h3 style={{ color: "orange", textAlign: "left", marginLeft: "30px" }}>New</h3>
-                                <h2>{pro.name}</h2>
+                                <h3 style={{ color: "orange", textAlign: "left", marginLeft: "30px", fontSize: "13px" }}>New</h3>
+                                <h1>{pro.name}</h1>
                                 <img src={pro.image} />
                                 <h5>{pro.EMI} <button>Buy</button></h5>
                             </div>))}
                         </div>}
                 </div>
             </div>
-
-            {
-                BuyIphone15Pro ? (
-                    <div className='iphone-15pro-slide-buy' onClick={() => setBuyIphone15Pro(!BuyIphone15Pro)}></div>
-                ) : ("")
-            }
-            {
-                <div>
-                    {
-                        BuyIphone15Pro ?
-                            <div className="main-box-iphone-15">
-                                <div className="iphone-pro-bar">
-                                    <button>iPhone 15 Pro</button>
-                                </div>
-                                <div>
-                                    <i id='close-button' onClick={() => setBuyIphone15Pro(!BuyIphone15Pro)} class="fa-solid fa-xmark"></i>
-                                </div>
-
-                                <div className="i-phone-buy-box">
-                                    <div className="iphone-flex-lft-rgt">
-                                        <div className='iphone-left'>
-                                            <button>iPhone 15pro</button>
-                                            <img src="asset/iphone-15gray.png" alt="" />
-                                        </div>
-
-
-                                        <div className='iphone-right'>
-                                            <div className="ipn-rgt-1">
-                                                <h5>NEW</h5>
-                                                <h1>IPhone-15Pro</h1>
-                                                <h6>From ₹21483.00/mo.Per Month with instant savings§§<br />  and No Cost EMI§Footnote or ₹134900.00Footnote‡ <button>Buy</button></h6>
-                                            </div>
-                                            <div className="ipn-rgt-2">
-
-                                                <h4>15.5 cm (6.1-inch) Super Retina XDR display footnote ¹ featuring ProMotion, Always-On and Dynamic Island</h4>
-                                            </div>
-                                            <div className="ipn-rgt-2">
-
-                                                <h4>15.5 cm (6.1-inch) Super Retina XDR display footnote ¹ featuring ProMotion, Always-On and Dynamic Island</h4>
-                                            </div>
-                                            <div className="ipn-rgt-2">
-
-                                                <h4>15.5 cm (6.1-inch) Super Retina XDR display footnote ¹ featuring ProMotion, Always-On and Dynamic Island</h4>
-                                            </div>
-                                            <div className="ipn-rgt-2">
-
-                                                <h4>15.5 cm (6.1-inch) Super Retina XDR display footnote ¹ featuring ProMotion, Always-On and Dynamic Island</h4>
-                                            </div>
-                                            <div className="ipn-rgt-2">
-
-                                                <h4>15.5 cm (6.1-inch) Super Retina XDR display footnote ¹ featuring ProMotion, Always-On and Dynamic Island</h4>
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                    <div className="iphone-flex-bottom">
-                                        <div className="iph-flex-1-btm">
-                                            <i class="fa-regular fa-credit-card" style={{ color: "#36ba1c" }}></i>
-                                            <div className="emi-pay">
-                                                <h4>Flexible ways to pay</h4>
-                                                <h5>No Cost EMI§ and EMI◊◊ available.</h5>
-                                            </div>
-                                        </div>
-                                        <div className="iph-flex-1-btm">
-
-                                        </div>
-                                        <div className="iph-flex-1-btm">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            :
-                            <div className="sidebar-dontshow"><i id='close-button' onClick={() => setBuyIphone15Pro(!BuyIphone15Pro)} class="fa-solid fa-xmark"></i></div>
-                    }
-                </div>
-            }
         </div>
 
     )
