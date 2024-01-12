614a4607b0510e11014fa18a907b825c809ceef6
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2024-01-12 19:50:53 +0530

    updates

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -2,31 +2,33 @@ import React from 'react';
 import './footer.css';
 
 const Footer = () => {
-  const listTitles = ['Shop and Learn','Apple Wallet','Account','Entertainment','Apple Store','For Business','For Education', 'For Healthcare','Apple Values','About Apple'];
+  const listTitles = ['Shop and Learn', 'Apple Wallet', 'Account', 'Entertainment', 'Apple Store', 'For Business', 'For Education', 'For Healthcare', 'Apple Values', 'About Apple'];
 
-  const listItems = [['Store','Mac','iPad','iPhone','Watch','AirPods','TV & Home','AirTag','Accessories','Gift Cards'],  ['Wallet'],['Manage Your Apple ID','Apple Store Account','iCloud.com'],['Apple One','Apple TV+','Apple Music','Apple Arcade','Apple Podcasts','Apple Books','App Store'],['Find a Store','Genius Bar','Today at Apple','Apple Camp','Apple Trade In','Ways to Buy','Recycling Programme','Order Status','Shopping Help'],['Apple and Business','Shop for Business'],['Apple and Education','Shop for Education','Shop for University'],['Apple in Healthcare','Health on Apple Watch'],['Accessibility','Education','Environment','Privacy','Supplier Responsiblity'],['Newsroom','Apple Leadership','Career Opportunities','Investors','Ethics & Complaince','Events','Contact Apple']]
+  const listItems = [['Store', 'Mac', 'iPad', 'iPhone', 'Watch', 'AirPods', 'TV & Home', 'AirTag', 'Accessories', 'Gift Cards'], ['Wallet'], ['Manage Your Apple ID', 'Apple Store Account', 'iCloud.com'], ['Apple One', 'Apple TV+', 'Apple Music', 'Apple Arcade', 'Apple Podcasts', 'Apple Books', 'App Store'], ['Find a Store', 'Genius Bar', 'Today at Apple', 'Apple Camp', 'Apple Trade In', 'Ways to Buy', 'Recycling Programme', 'Order Status', 'Shopping Help'], ['Apple and Business', 'Shop for Business'], ['Apple and Education', 'Shop for Education', 'Shop for University'], ['Apple in Healthcare', 'Health on Apple Watch'], ['Accessibility', 'Education', 'Environment', 'Privacy', 'Supplier Responsiblity'], ['Newsroom', 'Apple Leadership', 'Career Opportunities', 'Investors', 'Ethics & Complaince', 'Events', 'Contact Apple']]
   return (
-    <div className='footer-container'>
-      <div className='footer-lists-container'>
-        {
-          listTitles.map((title,i) => <div className='footer-single-list-container'><h5>{title}</h5>{listItems[i].map(item => <p>{item}</p>)}</div>)
-        }
+    <div className='footer-col'>
+      <div className='footer-container'>
+        <div className='footer-lists-container'>
+          {
+            listTitles.map((title, i) => <div className='footer-single-list-container'><h5>{title}</h5>{listItems[i].map(item => <p>{item}</p>)}</div>)
+          }
+        </div>
+        <br />
+        <p className='footer-text'>More ways to shop: <span className='blue-link'>Find an Apple Store</span> or <span className='blue-link'>other retailer</span> near you. Or call 000800 040 1966.</p>
+        <hr className='footer-hr-1' />
+        <div className='footer-bottom-div footer-text'>
+          <p >Copyright © 2023 Apple Inc. All rights reserved</p>
+          <ul >
+            <li>Privacy Policy</li>
+            <li>Terms of Use</li>
+            <li>Sales Policy</li>
+            <li>Legal</li>
+            <li>Site Map</li>
+          </ul>
+          <span>India</span>
+        </div>
+        <br />
       </div>
-      <br/>
-      <p className='footer-text'>More ways to shop: <span className='blue-link'>Find an Apple Store</span> or <span className='blue-link'>other retailer</span> near you. Or call 000800 040 1966.</p>
-      <hr className='footer-hr-1'/>
-      <div className='footer-bottom-div footer-text'>
-        <p >Copyright © 2023 Apple Inc. All rights reserved</p>
-        <ul >
-          <li>Privacy Policy</li>
-          <li>Terms of Use</li>
-          <li>Sales Policy</li>
-          <li>Legal</li>
-          <li>Site Map</li>
-        </ul>
-        <span>India</span>
-      </div>
-      <br/>
     </div>
   )
 }
--- website/src/components/Store.jsx
+++ website/src/components/Store.jsx
@@ -41,34 +41,34 @@ const Store = () => {
 
                 <div className='slider-content '>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-14-16-mac-nav-202301?wid=400&hei=260&fmt=png-alpha&.v=1670959891635" alt="" className="slide-imgs" />
-                        <div>Mac</div>
+                        <div className='store-cat'>Mac</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-iphone-nav-202309_GEO_US?wid=400&hei=260&fmt=png-alpha&.v=1692971740190" alt="" className="slide-imgs" />
-                        <div>iPhone</div>
+                        <div className='store-cat'>iPhone</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-ipad-nav-202210?wid=400&hei=260&fmt=png-alpha&.v=1664912135437" alt="" className="slide-imgs" />
-                        <div>iPad</div>
+                        <div className='store-cat'>iPad</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-watch-nav-202309?wid=400&hei=260&fmt=png-alpha&.v=1693703822208" alt="" className="slide-imgs" />
-                        <div>Apple Watch</div>
+                        <div className='store-cat'>Apple Watch</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-airpods-nav-202209?wid=400&hei=260&fmt=png-alpha&.v=1660676485885" alt="" className="slide-imgs" />
-                        <div>AirPods</div>
+                        <div className='store-cat'>AirPods</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-airtags-nav-202108?wid=400&hei=260&fmt=png-alpha&.v=1625783380000" alt="" className="slide-imgs" />
-                        <div>AirTag</div>
+                        <div className='store-cat'>AirTag</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-appletv-nav-202210?wid=400&hei=260&fmt=png-alpha&.v=1664628458484" alt="" className="slide-imgs" />
-                        <div>Apple TV 4k </div>
+                        <div className='store-cat'>Apple TV 4k </div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-homepod-nav-202301?wid=400&hei=260&fmt=png-alpha&.v=1670389216654" alt="" className="slide-imgs" />
-                        <div>HomePod</div>
+                        <div className='store-cat'>HomePod</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-accessories-nav-202309?wid=400&hei=260&fmt=png-alpha&.v=1692803114952" alt="" className="slide-imgs" />
-                        <div>Accessories</div>
+                        <div className='store-cat'>Accessories</div>
                     </div>
                     <div><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/store-card-13-holiday-giftcards-asit-agc-nav-202111?wid=400&hei=260&fmt=png-alpha&.v=1653339351054" alt="" className="slide-imgs" />
-                        <div>Apple Gift Card</div>
+                        <div className='store-cat'>Apple Gift Card</div>
                     </div>
                 </div>
 
